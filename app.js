cba917aff4874488a2b72b452fcf0226e52b1aed
dantheman <danogbo0@gmail.com>
2024-03-22 21:56:45 +0100

    json-basics done

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { products } = require('./data');
 
+app.get('/', (req, res) => {
+    res.send('<h1> Home Page </h1> <a href="/api/products"> all products </a>');
+})
+
+// app.get('/api/products', (req, res) => {
+//     res.json(products)
+// })
+
+// return specifc data e.g here, we arent returning description
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map( (product) => {
+        const {id, name, image} = product;
+        return {id, name, image}
+    } )
+
+    res.json(newProducts)
+})
+
+// single product
+app.get('/api/products/:prodId', (req, res) => {
+    const { prodId } = req.params;
+    const singleProduct = products.find( 
+        (product) => product.id === Number(prodId)
+    )
+    if(!singleProduct){
+        return res.status(404).send('Page Not Found')
+    }
+
+    return res.json(singleProduct)
+})
 
 // setup assets and middleware
 app.use(express.static('./public'))
 
-app.get('/', (req,res) => {
-    res.sendFile(path.resolve(__dirname, './views/index.html'))
+app.get('/api/products', (req,res) => {
+
 })
 
 app.all('*', (req, res) => {
@@ -15,5 +46,5 @@ app.all('*', (req, res) => {
 })
 
 app.listen(5001, () => {
-    console.log('listening on port 5000...');
+    console.log('listening on port 5001...');
 })
